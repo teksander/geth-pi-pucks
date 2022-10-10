d4e922867e99ed192726d3c021bf320c2dc07cae
HZHAO-ULB <hq.zhao@mail.mcgill.ca>
2022-10-10 16:32:34 +0200

    camera_calibration

--- control/robots/calibration/calibrate-camera.py
+++ control/robots/calibration/calibrate-camera.py
@@ -5,9 +5,11 @@ import numpy as py
 import os
 import time
 import sys
+from matplotlib import pyplot as plt
 sys.path.append('..')
 from rotation import Rotation
 from upcamera import UpCamera
+
 
 cam_int_reg_h = 100
 cam_rot = True
@@ -20,3 +22,12 @@ rot = Rotation(rotSpeed)
 
 image = cam.get_reading()
 cv2.imwrite('test_capture.jpg', image)
+def rgb_hist(image):
+    colors = ('b', 'g', 'r')
+    for i, col in enumerate(colors):
+        histr = cv2.calcHist([image],[i],None,[256],[0,256])
+        plt.plot(histr, color= col )
+        plt.xlim([0,256])
+    plt.show()
+
+rgb_hist(image)
