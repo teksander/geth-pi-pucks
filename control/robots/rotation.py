7388ed12cf377b0ce9ac2ac783b856e2f2c35ea0
HZHAO-ULB <hq.zhao@mail.mcgill.ca>
2022-10-12 15:43:39 +0200

    camera_calibration_and_temp

--- control/robots/rotation.py
+++ control/robots/rotation.py
@@ -56,7 +56,7 @@ class Rotation(object):
         """ This method runs in the background until program is closed """
 
         # Initialize I2C bus
-        I2C_CHANNEL = 4
+        I2C_CHANNEL = 12
         EPUCK_I2C_ADDR = 0x1f
         self.__bus = smbus.SMBus(I2C_CHANNEL)
         self.__i2c_address = EPUCK_I2C_ADDR
