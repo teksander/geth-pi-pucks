685faf0c4398f8dd787ce0dc6f2142c44525a75a
HZHAO-ULB <hq.zhao@mail.mcgill.ca>
2022-10-12 17:08:51 +0200

    camera_driven_navigation

--- control/robots/colorguidedwalk.py
+++ control/robots/colorguidedwalk.py
@@ -97,6 +97,7 @@ class WalktoColor(object):
                     distance_to_target = []
                     for local_feature in cur_feature:
                         distance_to_target.append(cross_entropy(this_color_feature, local_feature))
+                    print(distance_to_target)
                     dir = angle_to_target(distance_to_target, color_ce_threshold)
                     print("angular direction: ", dir)
                     if dir <= -1:
