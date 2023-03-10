1cafd99fac866e06c9cdaae3e3f8a5fa34e4358e
teksander <eksisepic.com@gmail.com>
2023-03-10 15:15:21 +0100

    install ready april tag

--- robots/arducam/apriltag-0.0.16/core/common/assert_with_unused.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ASSERT_WITH_UNUSED_H
-#define _ASSERT_WITH_UNUSED_H
-
-#include <assert.h>
-
-#define UNUSED(x) ((void)(x))
-
-#endif
--- robots/arducam/install.sh
+++ robots/arducam/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash  
 # Short script to install picamera and apriltags from source
 
+# Run as SUDO
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
 # Make sure this is a Pi-Puck
-if [[ $(whoami) != $ROBOTS_NAME && $SUDO_USER != $ROBOTS_NAME ]]; then
+if [[ $SUDO_USER != $ROBOTS_NAME ]]; then
 	echo "! Only run this script on robots !"
 	exit 1
 fi
@@ -20,7 +23,5 @@ if python3 -c "import apriltag" &> /dev/null
 then
     echo "Apriltag is already installed"
 else
-	cd apriltag
-    python3 setup.py install
-    cd ..
+    pip3  install apriltag-0.0.16-cp37-cp37m-linux_armv6l.whl
 fi
